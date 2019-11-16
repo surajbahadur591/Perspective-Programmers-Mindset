d7bf6fbad53c4c72deb7e209657deb917510ae9b
Suraj Bahadur <surajbahadur58@gmail.com>
2019-11-16 21:07:06 +0530

    new
    new

--- script.js
+++ script.js
@@ -22,6 +22,8 @@ function getting_quote() {
         // console.log(author);
         link2 = link2 + quote + "&author=" + author + "&fontSize=35&quoteFont=Lora&quoteFontColor=%234f4f4f&authorFont=Lato%20Black&authorFontColor=%23e5e5e5&enableHighlight=1&highlightColor=%23f0ea66&bgType=gradient&backgroundColor=%23000000&gradientType=linear&gradientColor1=%232193b0&gradientColor2=%236dd5ed&animation=none&client_id=27c45ae7006cf108fadf39fcd273c0c3f665d8fd3ebd";
         making_image();
+        
+        
     });
 }
 
